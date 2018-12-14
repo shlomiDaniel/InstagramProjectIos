d50d96a35f45dc2d0bcca5d4a3a527d3133ee2bc
SHLOMI <shlomi@shlomis-mac.local>
2018-12-15 01:53:28 +0200

    working  on camera page ...

--- ViewControllers/CameraViewController.swift
+++ ViewControllers/CameraViewController.swift
@@ -8,15 +8,47 @@
 
 import UIKit
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate , UINavigationControllerDelegate {
 
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var photo: UIImageView!
+    @IBOutlet weak var shareButoon: UIButton!
+    var selectedImage : UIImage?
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //imagePicker.delegate = self
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+        photo.isUserInteractionEnabled = true
+        photo.addGestureRecognizer(tapGestureRecognizer)
     }
-    
-
+    /////////////////////
+    let imagePicker = UIImagePickerController()
+    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer)
+    {
+        let tappedImagew = tapGestureRecognizer.view as! UIImageView
+        print("aaaaaaaaaaaaaaaaaaaaaaaaa")
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
+        // Your action
+    }
+    //////////////////////////
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        dismiss(animated: true, completion: nil)
+        //let originalImage: UIImagePickerController.InfoKey
+        if let image = info[.originalImage] as! UIImage?{
+            selectedImage = image
+            photo.image = image
+        }
+        //myImageView.image = infoPhoto
+    }
+//////////////////////////////////////
+    @IBAction func shareButtonClick(_ sender: Any) {
+        
+        
+    }
     /*
     // MARK: - Navigation
 
