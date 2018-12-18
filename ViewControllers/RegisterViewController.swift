9621772fcbb2c8d6de67aada3da2af90c42c19cb
Shany Golan <shany.golan@sap.com>
2018-12-18 17:23:07 +0200

    Upload url is now working

--- ViewControllers/RegisterViewController.swift
+++ ViewControllers/RegisterViewController.swift
@@ -12,25 +12,25 @@ import SVProgressHUD
 import FirebaseDatabase
 
 class RegisterViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
-
+    
     @IBOutlet weak var myImageView: UIImageView!
     
-   var image_selected : UIImage?
+    var image_selected : UIImage?
     //var firebaseDataBase : DatabaseReference!
-   var fireBaseDataBase = ModelFireBase()
+    var fireBaseDataBase = ModelFireBase()
     
     var ref : DatabaseReference!
     
-           //FirebaseApp.configure()
+    //FirebaseApp.configure()
     
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       imagePicker.delegate = self
-         ref = Database.database().reference()
-         //var image_selected : UIImage
+        imagePicker.delegate = self
+        ref = Database.database().reference()
+        //var image_selected : UIImage
         
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
         myImageView.isUserInteractionEnabled = true
@@ -49,12 +49,12 @@ class RegisterViewController: UIViewController,UIImagePickerControllerDelegate,U
         // Your action
     }
     
-  
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         dismiss(animated: true, completion: nil)
         //let originalImage: UIImagePickerController.InfoKey
         if let image = info[.originalImage] as! UIImage?{
-           
+            
             myImageView.image = image
             image_selected = image
         }
@@ -69,10 +69,7 @@ class RegisterViewController: UIViewController,UIImagePickerControllerDelegate,U
     @IBOutlet weak var password_txt: UITextField!
     
     @IBAction func register(_ sender: Any) {
-        
-        
         //fireBaseDataBase.regiser_new_user(mail: email_txt.text!, pass: password_txt.text!,userName: username_txt.text!)
-        
         
         Auth.auth().createUser(withEmail: email_txt.text!, password: password_txt.text!) { (user, error) in
             if error != nil{
@@ -90,53 +87,53 @@ class RegisterViewController: UIViewController,UIImagePickerControllerDelegate,U
             print(newUserRef.description())
             let pic_ref = self.ref.child("users").child("images")
             //pic_ref.setValue(["image": "image" ])
-        
-        
-        let user = Auth.auth().currentUser!
-        let uid = user.uid
-        let storage_ref = Storage.storage().reference(forURL: "gs://instagramfirebase-6b380.appspot.com").child("profile_image").child(uid)
-             newUserRef.setValue(["userName": self.username_txt.text! , "email" : self.email_txt.text!])
-            if let profile_imag =  self.image_selected!.jpegData(compressionQuality: 0.1){
-            storage_ref.putData(profile_imag, metadata: nil) { (metadata, error) in
-                if error != nil{
-                    return
-                }
-                
-                let profile_image_url = metadata?.storageReference?.downloadURL(completion: { (url, error) in
-                    var image_url = url?.absoluteString
-                    let newuserref = Database.database().reference().child("users").child(uid)
-                    // : https://instagramfirebase-6b380.firebaseio.com/
-                   // newuserref.setValue([ "profile_pic" :  image_url ])
-                   
-                       })
-                 }
             
+            let user = Auth.auth().currentUser!
+            let uid = user.uid
+            let storageRef = Storage.storage().reference(forURL: "gs://instagramfirebase-6b380.appspot.com").child("profile_image").child(uid)
+            newUserRef.setValue(["userName": self.username_txt.text! , "email" : self.email_txt.text!])
+            
+            if let profileImage =  self.image_selected!.jpegData(compressionQuality: 0.1){
+                storageRef.putData(profileImage, metadata: nil, completion: {
+                    (StorageMetadata, error) in
+                    
+                    if error == nil {
+                        // success image is uploaded
+                        storageRef.downloadURL { url, error in
+                            // success!
+                            let image_url = url?.absoluteURL
+                            print(image_url)
+                            
+                            //let newuserref = Database.database().reference().child("users").child(
+                            //newuserref.setValue([ "profile_pic" :  image_url ])
+                        }
+                    } else {
+                        // failed to upload image
+                    }
+                })
             }
-           
-       
-        
-        
-    
-//    @IBAction func dismissPage(_ sender: Any) {
-//        self.dismiss(animated: true, completion: nil)
-//    }
-    
-   
-//    @IBAction func profileImageButton(_ sender: Any) {
-//
-//
-////
-//        if UIImagePickerController.availableMediaTypes(for: .photoLibrary) != nil {
-//            picker.allowsEditing = false
-//            picker.sourceType = UIImagePickerController.SourceType.photoLibrary
-//            present(picker, animated: true, completion: nil)
-//        }
-//
-//
-    
-
+            
+            
+            //    @IBAction func dismissPage(_ sender: Any) {
+            //        self.dismiss(animated: true, completion: nil)
+            //    }
+            
+            
+            //    @IBAction func profileImageButton(_ sender: Any) {
+            //
+            //
+            //
+            //        if UIImagePickerController.availableMediaTypes(for: .photoLibrary) != nil {
+            //            picker.allowsEditing = false
+            //            picker.sourceType = UIImagePickerController.SourceType.photoLibrary
+            //            present(picker, animated: true, completion: nil)
+            //        }
+            //
+            //
+            
+            
         }}}
-  
-    
+
+
 
 
