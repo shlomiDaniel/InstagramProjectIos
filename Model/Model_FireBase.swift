7a4ff99a2ef63a5bc21f33abdabe1478bfb12d59
admin <admin@admins-mac-mini>
2019-03-09 23:00:34 +0200

    message

--- Model/Model_FireBase.swift
+++ Model/Model_FireBase.swift
@@ -22,7 +22,7 @@ class ModelFireBase{
     init(){
         FirebaseApp.configure()
         ref = Database.database().reference()
-        
+       
     }
     
     func regiser_new_user(mail : String  ,pass : String, callback : @escaping (Bool?)->Void)
@@ -213,6 +213,8 @@ class ModelFireBase{
     }
     
     func checkIfSignIn()->Bool{
+        //print("FB: Check if SignIn...")
+        //print (Auth.auth().currentUser != nil)
         return (Auth.auth().currentUser != nil)
     }
     
